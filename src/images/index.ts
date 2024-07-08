fbca748bd8eba0b503e1d1834b0d25115065decb
sh.shadanpoor <sh.shadanpoor@pixaldo.com>
2024-07-08 16:26:19 +0330

    initial

---
+++ src/images/index.ts
@@ -0,0 +1,21 @@
+import hamsterExchange from "./hamster-exchange.png";
+import binanceLogo from "./binance-logo.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./man.webp";
+import hamsterCoin from "./hamster-coin.png";
+import vayZanetoo from "./vay.mp4";
+
+export {
+    hamsterExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    hamsterCoin,
+    vayZanetoo
+};
